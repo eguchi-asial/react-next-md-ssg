2f443e78653ecc0402ff6d0106e1e2396fcf0e43
eguchi <eguchi@asial.co.jp>
2023-09-04 18:12:09 +0900

    take 10

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '../../../lib/prisma'
 
 export async function GET(_request: NextRequest) {
-  const markdowns = await prisma.markdowns.findMany()
+  const markdowns = await prisma.markdowns.findMany({ take: 10 })
 
   return NextResponse.json({
     markdowns
