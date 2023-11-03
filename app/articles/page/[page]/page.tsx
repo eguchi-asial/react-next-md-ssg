5690a1a16e4a4d37d5a9459fb9bed2e9c10eab86
eguchi <eguchi@asial.co.jp>
2023-11-03 10:39:37 +0900

    lint

--- app/articles/page/[page]/page.tsx
+++ app/articles/page/[page]/page.tsx
@@ -5,45 +5,44 @@ import { getMarkdownsByPage } from '../../../../lib/api'
 
 export default async function Articles({ params }: { params: { page: number } }) {
   const { markdowns, total = 0 } = await getPosts(params.page)
-  console.log(total)
-    return (
-      <div className={styles.contents}>
-        <AppHeader />
-        { params.page } ページ
+  return (
+    <div className={styles.contents}>
+      <AppHeader />
+      { params.page } ページ
 
-        { markdowns.length > 0 &&
-        <div className={styles.contents}>
-          <div className={styles['latest-reviews']}>
-            <div className={styles['items-wrapper']}>
-              <ul className={styles.items}>
-                { markdowns.map((post, index) =>
-                <li className="oddcolor" key={ index }>
-                  <Link
-                    className={styles.link}
-                    as={`/articles/${post.slug}`}
-                    href="/articles/[slug]"
-                  >
-                    <p className={styles.title}>{post.title}</p>
-                    <p className={styles.date}>{post.date}</p>
-                  </Link>
-                </li>
-                )}
-              </ul>
-            </div>
+      { markdowns.length > 0 &&
+      <div className={styles.contents}>
+        <div className={styles['latest-reviews']}>
+          <div className={styles['items-wrapper']}>
+            <ul className={styles.items}>
+              { markdowns.map((post, index) =>
+              <li className="oddcolor" key={ index }>
+                <Link
+                  className={styles.link}
+                  as={`/articles/${post.slug}`}
+                  href="/articles/[slug]"
+                >
+                  <p className={styles.title}>{post.title}</p>
+                  <p className={styles.date}>{post.date}</p>
+                </Link>
+              </li>
+              )}
+            </ul>
           </div>
         </div>
-      }
-
-        {
-          params.page > 1 &&
-          <Link href={`/articles/page/${Number(params.page) - 1}`} replace={true}>戻る</Link>
-        }
-        {
-          (params.page * 10) < total &&
-          <Link href={`/articles/page/${Number(params.page) + 1}`}>次へ</Link>
-        }
       </div>
-    )
+    }
+
+      {
+        params.page > 1 &&
+        <Link href={`/articles/page/${Number(params.page) - 1}`} replace={true}>戻る</Link>
+      }
+      {
+        (params.page * 10) < total &&
+        <Link href={`/articles/page/${Number(params.page) + 1}`}>次へ</Link>
+      }
+    </div>
+  )
 }
 
 const getPosts = async (page: number) => {
